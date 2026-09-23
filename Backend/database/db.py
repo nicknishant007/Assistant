2eee15ae1326a23f891f68e7d42e8e503840da21
Nishant Kumar <2022epb1240@iitrpr.ac.in>
2026-09-23 23:47:36 +0530

    db_pool

--- Backend/database/db.py
+++ Backend/database/db.py
@@ -5,7 +5,9 @@ from config.settings import settings
 
 engine = create_engine(
     settings.DATABASE_URL,
-    echo=True
+    echo=True,
+    pool_pre_ping=True,
+    pool_recycle=300
 )
 
 SessionLocal = sessionmaker(
