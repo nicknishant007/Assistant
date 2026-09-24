74359e5ceec604e6cab5c9e1af596059a1059c6a
Nishant Kumar <2022epb1240@iitrpr.ac.in>
2026-09-24 17:26:19 +0530

    auth.py(debug)

--- Backend/dependencies/auth.py
+++ Backend/dependencies/auth.py
@@ -6,14 +6,32 @@ from database.session import SessionLocal
 from database.models.user import User
 from utils.security import verify_token
 
-def get_current_user(access_token: str | None = Cookie(default=None), db: Session = Depends(get_db)):
+
+def get_current_user(
+    access_token: str | None = Cookie(default=None),
+    db: Session = Depends(get_db)
+):
+    print("COOKIE RECEIVED:", access_token)
+
     if not access_token:
+        print("NO COOKIE FOUND")
         raise HTTPException(401, "Not authenticated")
+
     payload = verify_token(access_token)
+
+    print("PAYLOAD:", payload)
+
     if not payload:
+        print("TOKEN INVALID")
         raise HTTPException(401, "Invalid or expired token")
-    user = db.query(User).filter(User.id == payload.get("sub")).first()
+
+    user = db.query(User).filter(
+        User.id == payload.get("sub")
+    ).first()
+
+    print("USER:", user)
+
     if not user:
         raise HTTPException(401, "User not found")
-    return user
 
+    return user
