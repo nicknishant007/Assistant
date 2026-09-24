22ee83040d479e4b9a203ef81f80ae444ac19765
Nishant Kumar <2022epb1240@iitrpr.ac.in>
2026-09-24 10:10:04 +0530

    whispre_fast

--- Backend/services/stt_service.py
+++ Backend/services/stt_service.py
@@ -1,13 +1,22 @@
 from faster_whisper import WhisperModel
 
-model = WhisperModel(
-    "base",
-    device="cpu",
-    compute_type="int8"
-)
+_model = None
+
+
+def get_model():
+    global _model
+    if _model is None:
+        _model = WhisperModel(
+            "base",
+            device="cpu",
+            compute_type="int8"
+        )
+    return _model
 
 
 def speech_to_text(audio_path: str) -> str:
+
+    model = get_model()
 
     segments, _ = model.transcribe(
         audio_path
